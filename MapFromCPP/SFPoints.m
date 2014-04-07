f845594908e85adfa527a5687ffd5df2ed1bf6ad
Sun PeiFeng <g7733pf@gmail.com>
2014-04-07 09:15:05 +0800

    把主要的语句放入drawRect之前

--- MapFromCPP/SFPoints.m
+++ MapFromCPP/SFPoints.m
@@ -7,7 +7,13 @@
 //
 
 #import "SFPoints.h"
+#import "SFViewController.h"
 
+@interface SFPoints()
+
+@property (weak, nonatomic)SFViewController *vc;
+
+@end
 @implementation SFPoints
 
 - (id)initWithFrame:(CGRect)frame
@@ -15,6 +21,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+    
     }
     return self;
 }
@@ -22,10 +29,10 @@
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-//- (void)drawRect:(CGRect)rect
-//{
-//
-//}
+- (void)drawRect:(CGRect)rect
+{
+    
+}
 
 
 - (void)drawWithStringsArray:(NSMutableArray *)stringsArray andSurfaceArray:(NSMutableArray *)surfaceArray andLineArray:(NSMutableArray *)lineArray
@@ -33,105 +40,47 @@
     for (id i in stringsArray)
     {
         static NSString *objectKind;
-        static BOOL isLoadingPieceOfPathFinished = NO;
-        
-        NSUInteger locationOfComma = [i rangeOfString:@","].location;
-        NSUInteger lengthOfComma = [i rangeOfString:@","].length;
-        
-        //        i stringByReplacingCharactersInRange:[NSMakeRange(locationOfComma, lengthOfComma)] withString:
-        
-        
+ 
         if ([i rangeOfString:@"surface"].length > 0)//当遇到表示surface开始的内容时，若在读文件第一行时则忽略，其余时候则将上一组点去组成path，保存至数组
         {
             if (surfaceArray.count >0)//将上一组Surface点去组成path，保存至数组
             {
+                
                 UIColor *color = [UIColor redColor];
                 [color setFill];
-                UIBezierPath *path = [UIBezierPath bezierPath];
-                path.lineWidth = 5.0;
-                path.lineCapStyle = kCGLineCapRound;
-                path.lineJoinStyle = kCGLineCapRound;
-                NSValue *val = [surfaceArray objectAtIndex:0];
-                CGPoint p = [val CGPointValue];
-                p.x = p.x/1000;
-                p.y = p.y/1000;
                 
-                [path moveToPoint:p];
+                UIBezierPath *path = [self setupedBezierPath];
+                
+                
+                [path moveToPoint:[self setupPointFromArray:surfaceArray atIndex:0]];
                 
                 for (NSUInteger i = 1; i<surfaceArray.count ; i++)
                 {
-                    if (i == surfaceArray.count-1)
-                    {
-                        NSValue *val1 = [surfaceArray objectAtIndex:i];
-                        CGPoint p1 = [val1 CGPointValue];
-                        p1.x = p1.x/1000;
-                        p1.y = p1.y/1000;
-                        [path addLineToPoint:p1];
-                        NSLog(@"x:%f  y:%f",p1.x,p1.y);
-                        [path closePath];
-                        [path stroke];
-                    }
-                    else if (i == 1)
-                    {
-                        NSValue *val1 = [surfaceArray objectAtIndex:i];
-                        CGPoint p1 = [val1 CGPointValue];
-                        p1.x = p1.x/1000;
-                        p1.y = p1.y/1000;
-                        
-                        [path addcurve];
-                    }
-                    else
-                    {
-                        NSValue *val1 = [surfaceArray objectAtIndex:i];
-                        CGPoint p1 = [val1 CGPointValue];
-                        p1.x = p1.x/1000;
-                        p1.y = p1.y/1000;
-                        [path addLineToPoint:p1];
-                    }
+                    //利用上面已经创建的path,添加点，并完成path，添加到view
+                    [self addPointsForPath:path fromArray:surfaceArray atIndex:i];
                 }
+                [surfaceArray removeAllObjects];
             }
             else if(lineArray.count >0)//将上一组line点去组成path，保存至数组
             {
                 UIColor *color = [UIColor redColor];
                 [color set];
-                UIBezierPath *path = [UIBezierPath bezierPath];
-                path.lineWidth = 5.0;
-                path.lineCapStyle = kCGLineCapRound;
-                path.lineJoinStyle = kCGLineCapRound;
-                NSValue *val = [surfaceArray objectAtIndex:0];
-                CGPoint p = [val CGPointValue];
-                p.x = p.x/1000;
-                p.y = p.y/1000;
-                [path moveToPoint:p];
+                
+                UIBezierPath *path = [self setupedBezierPath];
+
+                
+                [path moveToPoint:[self setupPointFromArray:surfaceArray atIndex:0]];
                 for (NSUInteger i = 1; i<surfaceArray.count ; i++)
                 {
-                    if (i == surfaceArray.count-1)
-                    {
-                        NSValue *val1 = [surfaceArray objectAtIndex:i];
-                        CGPoint p1 = [val1 CGPointValue];
-                        p1.x = p1.x/1000;
-                        p1.y = p1.y/1000;
-                        [path addLineToPoint:p1];
-                        [path closePath];
-//                        [path point]
-                        [path stroke];
-                    }
-                    else
-                    {
-                        NSValue *val1 = [surfaceArray objectAtIndex:i];
-                        CGPoint p1 = [val1 CGPointValue];
-                        p1.x = p1.x/1000;
-                        p1.y = p1.y/1000;
-                        [path addLineToPoint:p1];
-                    }
+                    [self addPointsForPath:path fromArray:lineArray atIndex:i];
                 }
+                [lineArray removeAllObjects];
                 
             }
             else//在读文件第一行时则忽略
             {
-                
+                objectKind = @"surface";
             }
-            objectKind = @"surface";
             
         }
         else if([i rangeOfString:@"line"].length > 0)//当遇到表示line开始的内容时
@@ -163,5 +112,43 @@
     
 }
 
+- (UIBezierPath *)setupedBezierPath
+{
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    path.lineWidth = 5.0;
+    path.lineCapStyle = kCGLineCapRound;
+    path.lineJoinStyle = kCGLineCapRound;
+    return path;
+}
+
+- (CGPoint)setupPointFromArray:(NSMutableArray *)array atIndex:(NSUInteger)index
+{
+    NSValue *val = [array objectAtIndex:index];
+    CGPoint p = [val CGPointValue];
+    p.x = p.x/2000;
+    p.y = p.y/2000;
+    NSLog(@"x:%f  y:%f",p.x,p.y);
+    return p;
+}
+
+- (UIBezierPath *)finalizePath:(UIBezierPath *)path
+{
+    [path closePath];
+    [path stroke];
+    return path;
+}
+
+- (void)addPointsForPath:(UIBezierPath *)path fromArray:(NSMutableArray *)array atIndex:(NSUInteger)index
+{
+    if (index == array.count-1)//当读取到最后一个元素时
+    {
+        [path addLineToPoint:[self setupPointFromArray:array atIndex:index]];
+        [self finalizePath:path];
+    }
+    else
+    {
+        [path addLineToPoint:[self setupPointFromArray:array atIndex:index]];
+    }
+}
 
 @end
--- MapFromCPP/SFViewController.m
+++ MapFromCPP/SFViewController.m
@@ -78,26 +78,7 @@
     SFPoints *pointV = [[SFPoints alloc]init];
     [pointV drawWithStringsArray:self.stringOfPointsArray andSurfaceArray:self.surfacePoints andLineArray:self.linePoints];
     
-    UIColor *color = [UIColor redColor];
-    [color set]; //设置线条颜色
     
-    UIBezierPath* aPath = [UIBezierPath bezierPath];
-    aPath.lineWidth = 5.0;
-    
-    aPath.lineCapStyle = kCGLineCapRound; //线条拐角
-    aPath.lineJoinStyle = kCGLineCapRound; //终点处理
-    
-    // Set the starting point of the shape.
-    [aPath moveToPoint:CGPointMake(100.0, 0.0)];
-    
-    // Draw the lines
-    [aPath addLineToPoint:CGPointMake(200.0, 40.0)];
-    [aPath addLineToPoint:CGPointMake(160, 140)];
-    [aPath addLineToPoint:CGPointMake(40.0, 140)];
-    [aPath addLineToPoint:CGPointMake(0.0, 40.0)];
-    [aPath closePath];//第五条线通过调用closePath方法得到的
-    
-    [aPath stroke];//Draws line 根据坐标点连线
 }
 
 
